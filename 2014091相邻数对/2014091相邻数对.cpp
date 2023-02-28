bd012c594e24d547faf5288cc4f05b2d3b696e4a
Heathcliff <2826190541@qq.com>
2023-02-28 17:57:25 +0800

    update

---
+++ 2014091相邻数对/2014091相邻数对.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n < 2)
+        cout << 0 << endl;
+    int nums[1005];
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+    sort(nums, nums + n);
+    int l = 0, r = 1;
+    int cnt = 0;
+    while (r != n) {
+        if (nums[r] - nums[l] == 1) {
+            cnt++;
+        }
+        l++;
+        r++;
+    }
+    cout << cnt << endl;
+}
+
---
+++ 2014092画图/2014092画图.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+const int N = 100;
+bool flag[N + 1][N + 1];
+
+int main()
+{
+    int n, x1, y1, x2, y2, sum = 0;
+    memset(flag, false, sizeof(flag));
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x1 >> y1 >> x2 >> y2;
+        sum += (x2 - x1) * (y2 - y1);
+        for (int i = x1; i < x2; i++)
+            for (int j = y1; j < y2; j++) {
+                if (flag[i][j])
+                    sum--;
+                flag[i][j] = true;
+            }
+    }
+    cout << sum << endl;
+    return 0;
+}
